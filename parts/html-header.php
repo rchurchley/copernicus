bdddd95f1cc9d23d4d9c3b112ed5e2b79fb01388
Ross Churchley <ross@rosschurchley.com>
2013-03-28 11:17:39 -0700

    add .colour-menu classes

--- parts/html-header.php
+++ parts/html-header.php
@@ -4,7 +4,7 @@
 		<title><?php bloginfo( 'name' ); ?><?php wp_title( '|' ); ?></title>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="shortcut icon" href="<?php echo esc_url( home_url( '/' ) ); ?>/favicon.ico"/>
+		<link rel="shortcut icon" href="<?php echo esc_url( home_url( '/' ) ); ?>favicon.ico"/>
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
