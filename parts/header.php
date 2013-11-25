a9b0f56586658404240110745736cffdc9226481
Ross Churchley <ross@rosschurchley.com>
2013-11-24 18:20:24 -0800

    Revert header link, slightly change position of `foldable` arrows attached to `<h2>`

--- parts/header.php
+++ parts/header.php
@@ -1,5 +1,8 @@
 <header class="site-header" role="banner">
-	<?php bloginfo( 'name' ); ?>
+	<a class="site-title" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+		<?php bloginfo( 'name' ); ?>
+	</a>
+
 	<nav class="site-navigation foldable" role="navigation">
 		<h3><?php _e( 'Menu', 'copernicus' ); ?></h3>
 		<?php 
