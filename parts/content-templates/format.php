a0e0457ee67046c627fa538fc01687810c3be984
Ross Churchley <ross@rosschurchley.com>
2013-12-14 12:15:04 -0800

    Possibly controversial change removing support for comments.

--- parts/content-templates/format.php
+++ parts/content-templates/format.php
@@ -1,13 +1,9 @@
-<article class="post">
-	<h1><a href="<?php esc_url( the_permalink() ); ?>" rel="bookmark">
-		<?php the_title(); ?>
-	</a></h1>
+<h1><a href="<?php esc_url( the_permalink() ); ?>" rel="bookmark">
+	<?php the_title(); ?>
+</a></h1>
 
-	<?php if( has_post_thumbnail() ) : ?>
-		<img class="alignleft" src="<?php echo wp_get_attachment_url(get_post_thumbnail_id()); ?>" alt=""/>
-	<?php endif;?>
+<?php if( has_post_thumbnail() ) : ?>
+	<img class="alignleft" src="<?php echo wp_get_attachment_url(get_post_thumbnail_id()); ?>" alt=""/>
+<?php endif;?>
 
-	<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'copernicus' ) ); ?>
-	
-</article>
-<?php comments_template( '', true ); ?>
+<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'copernicus' ) ); ?>
