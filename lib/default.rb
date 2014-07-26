fb726fe8f4dc37dbcb1895c4e297c041dce66a44
Ross Churchley <ross@rosschurchley.com>
2014-07-26 11:54:44 -0700

    Remove image_compressor

--- lib/default.rb
+++ lib/default.rb
@@ -1,8 +1,6 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
 
-require 'nanoc/filters/image_compressor'
-
 String.class_eval do
   def to_slug
     downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
