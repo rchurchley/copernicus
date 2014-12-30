a69843ef6e07019640823112b0da21e25a85694f
Ross Churchley <ross@rosschurchley.com>
2014-12-30 12:27:55 -0800

    Generalize: remove nanoc-specific items

--- lib/default.rb
+++
@@ -1,8 +0,0 @@
-# All files in the 'lib' directory will be loaded
-# before nanoc starts compiling.
-
-String.class_eval do
-  def to_slug
-    downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
-  end
-end
